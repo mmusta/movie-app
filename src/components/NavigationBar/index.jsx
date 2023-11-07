2f9c426fdc98c30754bb41ca943b696ff83a189d
Mücahit Mustafa Usta <mmustafausta@yahoo.com>
2023-11-07 23:20:26 +0300

    Update index.jsx

--- src/components/NavigationBar/index.jsx
+++ src/components/NavigationBar/index.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import S from './index.module.css';
 import { MenuOutlined } from '@ant-design/icons';
 import MyImage from '../../image/IMDb_website_logo.svg';
+import NavigationBarSearchBox from '../NavigationBarSearchBox';
+import Imdbpro from '../../image/IMDbPro_SmallFormatBlue.png';
 
 function NavigationBar() {
   return (
     <div className={S.NavigationBar}>
-      <img className={S.NavigationBarImage} src={MyImage} alt='example'/>
-      <button type='button' className={S.NavigationBarButton}><MenuOutlined />     Menu</button>
-      <form className={S.NavigationBarForm}><input placeholder='Search IMDB'type='search' className={S.NavigationBarSearchBox}></input><button></button></form>
-    
+      <img className={S.NavigationBarLogo} src={MyImage} alt='example'/>
+      <button type='button' className={S.NavigationBarButton}><MenuOutlined /><span>Menu</span></button>
+      <form className={S.NavigationBarForm}><NavigationBarSearchBox/></form>
+      <img className={S.NavigationBarImdbpro} src={Imdbpro} alt='example'/>
     </div>
   );
 }
